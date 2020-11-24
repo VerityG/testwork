f877b5ec07941e8242e572c94e11870b6b515265
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-11-25 01:07:01 +0300

    Update

--- Reservd/test.py
+++ Reservd/test.py
@@ -3,6 +3,7 @@ from random import randint
 import requests
 import json
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 headers = [{
@@ -32,22 +33,31 @@ def urls(x):
     return url_list
 
 
-url = 'https://www.reserved.com/ru/ru/new-in/men'
+url = 'https://www.reserved.com/ru/ru/man/clothes/shirts'
 r = requests.get(url, headers=headers[randint(0, 2)])
 if r.ok:
     urls = urls(r)
     data_info = []
     for i in urls:
-        r = requests.get(i, headers=headers[randint(0, 2)])
-        if r.ok:
-            soup = BeautifulSoup(r.content, 'html.parser')
-            h = soup.find('meta', attrs={'property': 'og:description'})
-            header = h.get('content')
-            p = soup.find('meta', attrs={'property': 'product:original_price:amount'})
-            price = p.get('content')
-            i = soup.find('meta', attrs={'property': 'og:image'})
-            img = i.get('content')
-            data_info.append({'header': header, 'price': price, 'img': img, })
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        driver = webdriver.Chrome(options=options)
+        driver.get(i)
+        page_source = driver.page_source
+        try:
+            color = driver.find_element_by_css_selector('button img[title]').get_attribute('title')
+        except:
+            color = ''
+        driver.quit()
+        soup = BeautifulSoup(page_source, 'lxml')
+        id = soup.find('span', attrs={'data-selen': "sku"}).text
+        name = soup.find('h1', attrs={'class': 'product-name'}).text
+        price = soup.find('meta', {'property': 'product:price:amount'}).get('content')
+        currency = soup.find('meta', {'property': 'product:price:currency'}).get('content')
+        img = soup.find('div', {'class': 'swipe gallery-swipe'}).find('img').get('src')
+        data_info.append({'id': id, 'name': name, 'price': price,'currency': currency, 'image': img, 'productlink': i,
+                          'brand': 'Reserved','color': color})
     h = codecs.open('data.json', 'w', 'utf-8')
     h.write(json.dumps(data_info, indent=2, ensure_ascii=False))
     h.close()
--- mainpy.py
+++ mainpy.py
@@ -1,42 +1,31 @@
-import codecs
-from random import randint
-import requests
-import json
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import json
+import codecs
 
 
-headers = [{
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/81.0.4044.138 Safari/537.36',
-    'Accept': 'text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
-    {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/80.0.3987.132 Safari/537.36',
-        'Accept': 'text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
-    {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/80.0.3987.132 Safari/537.36',
-        'Accept': 'text/html, application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-]
-
-
-def urls(x):
-    url_list = []
-    soup = BeautifulSoup(x.content, 'html.parser')
-    main_div = soup.find('section', class_=True)
-    div_list = main_div.findAll('article', attrs={'data-sku': True})
-    for div in div_list:
-        a = div.find('a', class_=True)
-        address = a.get('href')
-        url_list.append(address)
-    return url_list
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
 
 
 
-url = 'https://www.pullandbear.com/by/%D0%B4%D0%BB%D1%8F-%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD/%D0%BD%D0%BE%D0%B2%D0%B8%D0%BD%D0%BA%D0%B8/%D1%85%D1%83%D0%B4%D0%B8-%D1%81-%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%BE%D0%BC-%C2%AB%D1%80%D1%8D%D0%BF%D0%B5%D1%80-%D1%82%D1%83%D0%BF%D0%B0%D0%BA%C2%BB-c1030017537p502559114.html?cS=800'
-r = requests.get(url, headers=headers[randint(0, 2)])
-if r.ok:
-    soup = BeautifulSoup(r.content, 'html.parser')
-    # main = soup.find('script', attrs={'type': 'application/ld+json'})
-    data = json.loads(soup.find('script', attrs={'type': 'application/ld+json'}).string)
-    print(data)
+data_info = []
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.reserved.com/ru/ru/yw284-99x/yw284-99x-t01-sweter-m-re')
+page_source = driver.page_source
+try:
+    color = driver.find_element_by_css_selector('button img[title]').get_attribute('title')
+except:
+    color = ''
+driver.quit()
+soup = BeautifulSoup(page_source, 'lxml')
+id = soup.find('span', attrs={'data-selen':"sku"}).text
+name = soup.find('h1', attrs={'class': 'product-name'}).text
+price = soup.find('meta', {'property': 'product:price:amount'}).get('content')
+currency = soup.find('meta', {'property': 'product:price:currency'}).get('content')
+img = soup.find('div', {'class': 'swipe gallery-swipe'}).find('img').get('src')
+data_info.append(
+    {'id': id, 'Name': name, 'Price': price, 'currency': currency, 'Image': img, 'ProductLink': '', 'Brand': 'Reserved', 'Color': color})
+print(data_info)
