029f7a81d8597b4d5d9de2ebf42ba36a4a468e03
User <ivan.vasiliuk@gmail.com>
2021-01-10 23:30:31 +0300

    Upd

--- Stradi/parsS.py
+++ Stradi/parsS.py
@@ -5,6 +5,7 @@ import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import lxml
 
 
 headers = [{
@@ -24,19 +25,24 @@ headers = [{
 
 def urls(x):
     url_list = []
-    soup = BeautifulSoup(x.content, 'html.parser')
-    main_div = soup.find('noscript')
-    div_list = main_div.findAll('a', attrs={'href': True})
-    for div in div_list:
-        address = div.get('href')
-        url_list.append(address)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    driver = webdriver.Chrome(options=options)
+    driver.get(x)
+    time.sleep(15)
+    driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+    page_source = driver.find_elements_by_css_selector('a#hrefRedirectProduct')
+    for i in page_source:
+        url_list.append(i.get_attribute('href'))
+    driver.quit()
     return url_list
 
 
-url = 'https://www.stradivarius.com/by/%D0%BD%D0%BE%D0%B2%D0%BE%D0%B9-%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%86%D0%B8%D0%B8/%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0/%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C-%D0%BF%D0%BE-%D1%82%D0%BE%D0%B2%D0%B0%D1%80%D1%83/%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE/%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C-%D0%B2%D1%81%D0%B5-c1020140016.html'
+url = 'https://www.stradivarius.com/by/%D0%BD%D0%BE%D0%B2%D0%BE%D0%B9-%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%86%D0%B8%D0%B8/%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0/%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C-%D0%BF%D0%BE-%D1%82%D0%BE%D0%B2%D0%B0%D1%80%D1%83/%D1%80%D1%83%D0%B1%D0%B0%D1%88%D0%BA%D0%B8/%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C-%D0%B2%D1%81%D0%B5-c1020047030.html'
 r = requests.get(url, headers=headers[randint(0, 2)])
 if r.ok:
-    urls = urls(r)
+    urls = urls(url)
     data_info = []
     for i in urls:
         options = webdriver.ChromeOptions()
@@ -44,6 +50,7 @@ if r.ok:
         options.add_argument('--incognito')
         driver = webdriver.Chrome(options=options)
         driver.get(i)
+        driver.maximize_window()
         time.sleep(10)
         page_source = driver.page_source
         driver.quit()
@@ -57,6 +64,6 @@ if r.ok:
                               'article': data['mpn'], 'color': color})
         except KeyError:
             continue
-    h = codecs.open('data_plt.json', 'w', 'utf-8')
+    h = codecs.open('data_rbsh.json', 'w', 'utf-8')
     h.write(json.dumps(data_info, indent=2, ensure_ascii=False))
     h.close()
